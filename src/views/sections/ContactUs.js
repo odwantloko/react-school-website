0a770538454fd17203d0b6c13eeebd593f37a14e
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-07-23 18:17:55 +0200

    Added Map and address

--- src/views/sections/ContactUs.js
+++ src/views/sections/ContactUs.js
@@ -1,38 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Container, Row, Col } from "reactstrap";
+import { MapContainer } from "./MapContainer";
 
-// reactstrap components
-import { Button, Container, Row, Col, UncontrolledTooltip } from "reactstrap";
+const apiMaps = "AIzaSyBgULUwucUDVGOu_12brCamNiaUgQKUc-I";
 
-// core components
+const mapStyles = {
+  width: '65%',
+  height: '20rem',
+  position: 'relative'
 
-function ContactUs() {
+};
+
+
+export class ContactUs extends React.Component {
+render () {
   return (
-    <>
+    
       <div
-        className="section section-download"
+        className="section"
         data-background-color="black"
         id="contact-us-section"
+        style={{minHeight: '50rem'}}
+
       >
-        <Container>
-          <Row className="justify-content-md-center">
-            <Col className="text-center" lg="8" md="12">
-              <h3 className="title">Do you love this Bootstrap 4 UI Kit?</h3>
-              <h5 className="description">
-                Cause if you do, it can be yours for FREE. Hit the button below
-                to navigate to Creative Tim or Invision where you can find the
-                kit in HTML or Sketch/PSD format. Start a new project or give an
-                old Bootstrap project a new look!
-              </h5>
-            </Col>
-            <Col className="text-center" lg="8" md="12">
-              
-            </Col>
-          </Row>
+        <Row className="justify-content-md-center">
+          <Col lg="8" md="12">
+            <h3 className="title">Want to get in touch with us?</h3>
+            <h5 className="description"></h5>
+              <p>            
+                <i className="fa fa-phone" aria-hidden="true"></i> &nbsp;
+                <b>Telephone number: </b>(021) 376 1067
+              </p>
+              <p>
+                <i className="fa fa-envelope" aria-hidden="true"></i> &nbsp;
+                <b>E-mail:</b> example@example.co.za
+              </p>
+              <p>
+                <i className="fa fa-map" aria-hidden="true"></i> &nbsp;
+                <b>Address:<br/> </b>Oval North Crescent
+                <br/>
+                Beacon Valley 
+                <br/>
+                Mitchell's Plain
+                <br/>
+                7785
+                <br/>
+              </p>
+              <Container>
+                <Map
+                    google={this.props.google}
+                    zoom={16.9}
+                    style={mapStyles}
+                    initialCenter={{ lat: -34.037523, lng: 18.631795 }}>
+                    
+                    <Marker position={{ lat: -34.037523, lng: 18.631795 }} />
+                </Map>  
+
+              </Container>
+          </Col>            
+        </Row>
+
+      
          
-        </Container>
       </div>
-    </>
+
+
+    
   );
 }
 
-export default ContactUs;
+}
+
+export default GoogleApiWrapper({
+  apiKey: apiMaps
+})(ContactUs);
+
+const map = {
+  //position: 
+}
