3228dbd1e2bcb43944fdecdf2d8db38eed36937b
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-07-27 19:13:06 +0200

    made the main pages less wordy

--- src/views/sections/Explore.js
+++ src/views/sections/Explore.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // reactstrap components
 import { Button, Container, Row, Col, UncontrolledTooltip } from "reactstrap";
+import { Link } from "react-router-dom";
 
 // core components
 
@@ -18,34 +19,19 @@ function Explore() {
               <h3 className="title">What we can offer you</h3>
                 <h4><b>Academic</b></h4>
                   <h5 style={text}>We offer the following subjects to choose from:</h5>   
-                  <h5 style={text}><b>Compulsory Subjects</b></h5>   
-                  <ul style={list}>
-                    <li>English Home Language/English First Addition Language</li>
-                    <li>Afrikaans Home Language/English First Addition Language</li>
-                    <li>Life Orientation</li>
-                    <li>Mathematics/Mathematical Literacy/Technical Mathematics</li>
-                  </ul>
 
-                  <h5 style={text}><b>Subjects To Choose from</b></h5>   
+                  <h5 style={text}><b>Choices</b></h5>   
                   <ul style={list}>
                    <li>Engineering Graphics & Design</li>
                    <li>Electrical Technology</li>
                    <li>Mechanical Technology</li>
                    <li>Civil Technology</li>
-                   <li>Life Sciences</li>
-                   <li>Physical Sciences</li>
-                   <li>Accounting</li>
-                   <li>Business Studies</li>
-                   <li>Accounting</li>
-                   <li>Computer Applications Technology</li>
+                   <Button style={{backgroundColor:'#2c2c2c' ,color:'#fff'}}>
+              <Link to="/explore"> READ MORE  <i className="fa fa-arrow-right"></i></Link>
+            </Button>
                   </ul>
-
-                <h4 className="title">Non-Academic</h4>
-                  <h5 style={text}>[insert list of Activities]</h5>
-
-            </Col>
-            <Col className="text-center" lg="8" md="12">
-              
+             
+           
             </Col>
           </Row>
         </div>
